3aedf5af4ae8a2b5f59013c6fd6f1e965513b251
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-17 14:52:08 +0800

    add code to open and close the books.csv file

--- assignment1.py
+++ assignment1.py
@@ -6,8 +6,11 @@ GitHub URL: https://github.com/JCUS-CP1404/assignment-1-JoshuaWibowo
 
 
 def main():
-    """..."""
+    """Main function"""
+    book_data = open("books.csv", "a")
     print("Reading Tracker 1.0 - by Joshua Wibowo")
+
+    book_data.close()
 
 
 if __name__ == '__main__':
