e210f4a2195ae687ce8257d612873d5ef488da4c
JoshuaWibowo <joshuatimothygratio.wibowo@my.jcu.edu.au>
2022-04-17 14:42:48 +0800

    create assignment1 file

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+"""
+Name: Joshua Timothy Gratio Wibowo
+Date started: 17/4/2022
+GitHub URL: https://github.com/JCUS-CP1404/assignment-1-JoshuaWibowo
+"""
+
+
+def main():
+    """..."""
+    print("Reading Tracker 1.0 - by Joshua Wibowo")
+
+
+if __name__ == '__main__':
+    main()
